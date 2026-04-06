fc038e267126653846772c41b09d6266b218a52c
vinicius <vinicius.siminio@aluno.senai.br>
2026-04-06 13:38:28 -0300

    vini

--- chat-app/public/script.js
+++ chat-app/public/script.js
@@ -2,18 +2,18 @@ const socket = io();
 const chat = document.getElementById('chat');
 const messageInput = document.getElementById('message');
 const userList = document.getElementById('user-list');
+const userCount = document.getElementById('user-count');
 
-const username = localStorage.getItem("username");
-const userphoto = localStorage.getItem("userphoto");
+const name = localStorage.getItem("username");
+const photo = localStorage.getItem("userphoto");
 
-// Envia nome E foto ao entrar
-socket.emit('join', { name: username, photo: userphoto });
+// Envia os dados do perfil ao conectar
+socket.emit('join', { name, photo });
 
 function sendMessage() {
     const text = messageInput.value;
     if (text.trim() !== "") {
-        // Envia a foto junto com a mensagem
-        socket.emit('message', { text: text, photo: userphoto });
+        socket.emit('message', text);
         messageInput.value = '';
     }
 }
@@ -22,24 +22,27 @@ socket.on('message', (data) => {
     const div = document.createElement('div');
     div.classList.add('msg');
     div.innerHTML = `
-        <div class="msg-header">
-            <img src="${data.photo}" class="msg-avatar">
-            <span>${data.user}</span>
+        <div class="msg-content">
+            <div class="msg-user-info">
+                <img src="${data.photo}" class="msg-avatar">
+                <span class="msg-name">${data.user}</span>
+            </div>
+            <span>${data.text}</span>
         </div>
-        <div>${data.text}</div>
     `;
     chat.appendChild(div);
     chat.scrollTop = chat.scrollHeight;
 });
 
-socket.on('updateUserList', (users) => {
+socket.on('updateUserList', (list) => {
+    userCount.innerText = list.length;
     userList.innerHTML = '';
-    users.forEach(user => {
+    list.forEach(u => {
         const li = document.createElement('li');
         li.classList.add('user-info');
         li.innerHTML = `
-            <img src="${user.photo}" class="avatar">
-            <span>${user.name}</span>
+            <img src="${u.photo}" class="avatar">
+            <span>${u.name}</span>
         `;
         userList.appendChild(li);
     });
--- chat-app/server.js
+++ chat-app/server.js
@@ -1,22 +1,40 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
 let users = {}; 
 
 io.on('connection', (socket) => {
     socket.on('join', (data) => {
-        // Agora data é { name: '...', photo: '...' }
+        // Guarda o objeto completo: nome e foto
         users[socket.id] = { name: data.name, photo: data.photo };
         io.emit('updateUserList', Object.values(users));
     });
 
-    socket.on('message', (data) => {
-        io.emit('message', {
-            user: users[socket.id].name,
-            photo: data.photo,
-            text: data.text
-        });
+    socket.on('message', (text) => {
+        const userData = users[socket.id];
+        if (userData) {
+            io.emit('message', {
+                user: userData.name,
+                photo: userData.photo,
+                text: text
+            });
+        }
     });
 
     socket.on('disconnect', () => {
         delete users[socket.id];
         io.emit('updateUserList', Object.values(users));
     });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
