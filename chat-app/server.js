ac7277ae7f5a5d5410b2890c328225a9892c15a8
vinicius <vinicius.siminio@aluno.senai.br>
2026-04-13 14:14:17 -0300

    kkkkkkkkkkkkk

--- chat-app/server.js
+++ chat-app/server.js
@@ -68,5 +68,6 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`🚀 Servidor voando na porta ${PORT}`));
+
